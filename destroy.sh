103d111f47f156b2fffe3cf5c858e78040989432
Aashish Sharma <aashishsharma@Aashishs-MBP-2.attlocal.net>
2024-01-05 11:37:55 -0600

    add destroy.sh

---
+++ destroy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# -xeo prints all executed commands, exit if any fails, prevent error from being masked
+set -x
+set -eo pipefail
+
+echo "Uninstalling all brew packages"
+brew remove --force $(brew list --formula)
+
+echo "Uninstalling all brew packages"
+brew remove cask --force $(brew list --formula)
