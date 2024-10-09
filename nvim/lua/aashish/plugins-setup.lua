7ebc73268eeb165b8524e0f47c42f0bcd1632dba
aashish2057 <aashish.sharma@shelfengine.com>
2024-10-09 06:33:17 -0500

    fix luasnip

--- nvim/lua/aashish/plugins-setup.lua
+++ nvim/lua/aashish/plugins-setup.lua
@@ -53,7 +53,13 @@ local plugins = {
 	"hrsh7th/cmp-cmdline",
 
 	-- snippets
-	"L3MON4D3/LuaSnip",
+	{
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp"
+    },
 	"saadparwaiz1/cmp_luasnip",
 	"rafamadriz/friendly-snippets",
 
