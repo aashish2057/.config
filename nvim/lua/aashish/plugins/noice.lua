3a22e9de03a3e444afad5f99f75eab0c1dffa527
Aashish Sharma <aashish2057@gmail.com>
2024-10-18 09:00:36 -0500

    fix noice width not increasing

--- nvim/lua/aashish/plugins/noice.lua
+++ nvim/lua/aashish/plugins/noice.lua
@@ -5,24 +5,12 @@ end
 
 noice.setup({
 	lsp = {
-		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 		override = {
 			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 			["vim.lsp.util.stylize_markdown"] = true,
 			["cmp.entry.get_documentation"] = true,
 		},
 	},
-	-- you can enable a preset for easier configuration
-	-- cmdline = {
-	-- 	view = "cmdline",
-	-- },
-	-- presets = {
-	-- 	bottom_search = true, -- use a classic bottom cmdline for search
-	-- 	command_palette = true, -- position the cmdline and popupmenu together
-	-- 	long_message_to_split = true, -- long messages will be sent to a split
-	-- 	inc_rename = false, -- enables an input dialog for inc-rename.nvim
-	-- 	lsp_doc_border = false, -- add a border to hover docs and signature help
-	-- },
 	routes = {
 		{
 			filter = {
@@ -36,20 +24,14 @@ noice.setup({
 			view = "mini",
 		},
 	},
-	-- presets = {
-	-- 	command_palette = true,
-	-- 	long_message_to_split = true,
-	-- 	inc_rename = true,
-	-- },
 	views = {
 		cmdline_popup = {
 			position = {
 				row = 5,
 				col = "50%",
 			},
-			size = {
-				width = 60,
-				height = "auto",
+			win_options = {
+				winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
 			},
 		},
 		popupmenu = {
@@ -57,10 +39,6 @@ noice.setup({
 			position = {
 				row = 8,
 				col = "50%",
-			},
-			size = {
-				width = 60,
-				height = 10,
 			},
 			border = {
 				style = "rounded",
